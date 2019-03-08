cbbea56513d30ff174881ece5cf0d35975018ff6
“ALEX <alexgtr84@gmail.com>
2019-03-08 11:08:53 +0100

    Add app changes

--- src/App.js
+++ src/App.js
@@ -9,27 +9,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <h1>HI THERE</h1>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-=======
         <TopBar />
         <ActiveClient />
         <SleepingClient />
->>>>>>> 6d9fa7b7696ad25a847f0884edeba1f4bd4145ce
       </div>
     );
   }
