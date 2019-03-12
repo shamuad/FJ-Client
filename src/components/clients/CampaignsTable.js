c830bd983549344bb8f102bf8422480751543dbf
Joey Grolleman <joeygrolleman@gmail.com>
2019-03-12 12:14:46 +0100

    list of campaign displayed in dropdown

--- src/components/clients/CampaignsTable.js
+++ src/components/clients/CampaignsTable.js
@@ -20,12 +20,14 @@ const CampaignTitles = (props) => (
     {
     getCampaigns {
       google{
+        id
         name
         ads {
           name
         }
       },
       facebook{
+        id
         name,
         ads {
           name
@@ -38,9 +40,26 @@ const CampaignTitles = (props) => (
   {({ loading, error, data }) => {
       if (loading) return "Loading...";
       if (error) return `Error! ${error.message}`;
-      console.log(data)
+      const rows = []
+      data.getCampaigns.google.map(campaign => rows.push(createData(campaign.id,campaign.name, campaign.ads.length)))
+      data.getCampaigns.facebook.map(campaign => rows.push(createData(campaign.id,campaign.name, campaign.ads.length)))
       return (
-       null
+        <TableBody>
+          {rows.map(row => (
+            <TableRow key={row.id}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              {/* <TableCell align="right">{row.target}</TableCell> */}
+              {/* <TableCell align="right"><FontAwesomeIcon icon={faInfo} />  {row.info}</TableCell> */}
+              {/* <TableCell align="right"> */}
+                {/* <FontAwesomeIcon icon={faFacebookF} /><span>  </span>
+                <FontAwesomeIcon icon={faYoutube} /> */}
+              {/* </TableCell> */}
+              <TableCell align="right">{row.cCount}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
       );
     }}
   
@@ -58,20 +77,12 @@ const styles = theme => ({
   },
 });
 
-let id = 0;
-function createData(name, target, info, social, cCount, rDate) {
-  id += 1;
-  return { id, name, target, info, social, cCount, rDate };
+
+function createData(id, name, cCount) {
+  return { id, name, cCount };
 }
 
-const rows = [
-  createData('Quaker Video 1', '%80 Campain Target', '960/1200 leads generated','', 4.0, 5),
-  createData('Quaker Video 2', '%30 Campain Target', '22.500/75.000 euro purchase value','', 4.3, 12),
-  createData('Quaker Lore', '%30 Campain Target', '800.000/1.000.000 unique views', '', 6.0, 25),
-  createData('Quaker Ipsum', '%30 Campain Target', '960/1200 leads generated', '', 4.3, 15),
-  createData('Quaker Lorem', '%30 Campain Target', '960/1200 leads generated', '', 3.9, 18),
-];
-
+const rows = []
 
 
 
@@ -81,31 +92,27 @@ function CampaignsTable(props) {
   return (
     <Paper className={classes.root}>
       <Table className={classes.table}>
-      <CampaignTitles />
         <TableHead>
           <TableRow>
             <TableCell>Title</TableCell>
-            <TableCell align="right">Campaign Target</TableCell>
-            <TableCell align="right">Info</TableCell>
-            <TableCell align="right">Social Media</TableCell>
+            {/* <TableCell align="right">Social Media</TableCell> */}
             <TableCell align="right">Count</TableCell>
-            <TableCell align="right">Remaining Day</TableCell>
           </TableRow>
         </TableHead>
+        <CampaignTitles />
         <TableBody>
           {rows.map(row => (
             <TableRow key={row.id}>
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.target}</TableCell>
-              <TableCell align="right"><FontAwesomeIcon icon={faInfo} />  {row.info}</TableCell>
-              <TableCell align="right">
-                <FontAwesomeIcon icon={faFacebookF} /><span>  </span>
-                <FontAwesomeIcon icon={faYoutube} />
-              </TableCell>
+              {/* <TableCell align="right">{row.target}</TableCell> */}
+              {/* <TableCell align="right"><FontAwesomeIcon icon={faInfo} />  {row.info}</TableCell> */}
+              {/* <TableCell align="right"> */}
+                {/* <FontAwesomeIcon icon={faFacebookF} /><span>  </span>
+                <FontAwesomeIcon icon={faYoutube} /> */}
+              {/* </TableCell> */}
               <TableCell align="right">{row.cCount}</TableCell>
-              <TableCell align="right">{row.rDate}</TableCell>
             </TableRow>
           ))}
         </TableBody>
