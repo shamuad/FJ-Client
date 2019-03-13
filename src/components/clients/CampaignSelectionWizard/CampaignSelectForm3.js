e2273a22c7a7a628cdbd5d84cfe584a9812c0fa3
ARI DINAR <dinarari@gmail.com>
2019-03-13 11:54:30 +0100

    selection-wizard

--- src/components/clients/CampaignSelectionWizard/CampaignSelectForm3.js
+++ src/components/clients/CampaignSelectionWizard/CampaignSelectForm3.js
@@ -1,87 +1,149 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import OndemandVideoIcon from '@material-ui/icons/OndemandVideo';
+import DeleteIcon from '@material-ui/icons/Delete';
+import TextField from '@material-ui/core/TextField';
 
 const styles = theme => ({
-  card: {
-    minWidth: 475,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
-  },
-  dense: {
-    marginTop: 16,
-  },
-  menu: {
-    width: 200,
-  },
+    root: {
+        flexGrow: 1,
+        maxWidth: 752,
+    },
+    demo: {
+        backgroundColor: theme.palette.background.paper,
+    },
+    title: {
+        // margin: `${theme.spacing.unit * 4}px 0 ${theme.spacing.unit * 2}px`,
+    },
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    textField: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+        width: 50,
+    },
+    dense: {
+        marginTop: 19,
+    },
+    menu: {
+        width: 200,
+    },
 });
 
-function Review(props) {
-  const { classes } = props;
-  const bull = <span className={classes.bullet}>•</span>;
-
-  return (
-
-    
-
-    <Card className={classes.card}>
-      <CardContent>
-      <Grid container spacing={9}>
-        <Grid item xs={3} sm={3}>
-        <form className={classes.container} noValidate autoComplete="off">
-        <TextField
-          id="outlined-name"
-          className={classes.textField}
-          value=""
-          margin="normal"
-          variant="outlined"
-        />
-      </form>
-        </Grid>
-        <Grid item xs={3} sm={3}><Typography className={classes.title} color="textSecondary" gutterBottom>
-          Title
-        </Typography></Grid>
-        <Grid item xs={3} sm={3}>
-        <CardMedia
-          className={classes.media}
-          image="/static/images/cards/paella.jpg"
-          title="Paella dish"
-        />
-        </Grid>
-        </Grid>
-      </CardContent>
-    </Card>
-
-  );
+function generate(element) {
+    return [0, 1, 2].map(value =>
+        React.cloneElement(element, {
+            key: value,
+        }),
+    );
 }
 
-Review.propTypes = {
-  classes: PropTypes.object.isRequired,
+class InteractiveList extends React.Component {
+    state = {
+        dense: false,
+        secondary: false,
+    };
+
+    render() {
+        const { classes } = this.props;
+        const { dense, secondary } = this.state;
+
+        return (
+            <div className={classes.root}>
+                <Grid container spacing={16}>
+                    <Grid item xs={12} md={6}>
+                        <Typography variant="h6" className={classes.title}>
+                            Facebook Campaign
+                        </Typography>
+                        <div className={classes.demo}>
+                            <List dense={dense}>
+                                {generate(
+                                    <ListItem>
+                                        <ListItemAvatar>
+                                            <Avatar>
+                                                <OndemandVideoIcon />
+                                            </Avatar>
+                                        </ListItemAvatar>
+                                        <ListItemText
+                                            primary="Single-line item"
+                                            secondary={secondary ? 'Secondary text' : null}
+                                        />
+                                        <ListItemSecondaryAction>
+                                            <TextField
+                                                id="filled-bare"
+                                                className={classes.textField}
+                                                defaultValue=""
+                                                margin="normal"
+                                                variant="filled"
+                                            />
+                                        </ListItemSecondaryAction>
+                                    </ListItem>,
+                                )}
+                            </List>
+                        </div>
+                    </Grid>
+
+
+                    <Grid item xs={12} md={6}>
+                        <Typography variant="h6" className={classes.title}>
+                            Google Campaign
+                        </Typography>
+                        <div className={classes.demo}>
+                            <List dense={dense}>
+                                {generate(
+                                    <ListItem>
+                                        <ListItemAvatar>
+                                            <Avatar>
+                                                <OndemandVideoIcon />
+                                            </Avatar>
+                                        </ListItemAvatar>
+                                        <ListItemText
+                                            primary="Videos-for google"
+                                            secondary={secondary ? 'Secondary text' : null}
+                                        />
+                                        <ListItemSecondaryAction>
+                                            <TextField
+                                                id="filled-bare"
+                                                className={classes.textField}
+                                                defaultValue=""
+                                                margin="normal"
+                                                variant="filled"
+                                            />
+                                        </ListItemSecondaryAction>
+                                    </ListItem>,
+                                )}
+                            </List>
+                        </div>
+                    </Grid>
+
+
+                </Grid>
+            </div>
+
+
+
+        );
+    }
+}
+
+InteractiveList.propTypes = {
+    classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(Review);
+export default withStyles(styles)(InteractiveList);
